dec3b3022f20433082aaf87ec218c322f47d149e
leonardo furtado <srleonardofurtado@gmail.com>
2021-10-20 08:31:57 -0300

    feat: setup docker and postgreesql.

--- company/settings.py
+++ company/settings.py
@@ -83,11 +83,16 @@ WSGI_APPLICATION = 'company.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
+
 
 
 # Password validation
