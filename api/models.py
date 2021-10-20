6dea6e6261fb8d9c075ae06be8b0eb6cbcac5a8e
leonardo furtado <srleonardofurtado@gmail.com>
2021-10-20 00:44:30 -0300

    feat: add initial models Employee and Company.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    """
+    name:
+    trading_name:
+    situation:
+    updated:
+    created:
+    """
+    ACTIVE = 'A'
+    INACTIVE = 'I'
+    EXCLUDED = 'E'
+    SITUATION_CHOICES = [
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+        (EXCLUDED, 'Excluded'),
+    ]
+    name = models.CharField(max_length=150, verbose_name='Name')
+    trading_name = models.CharField(max_length=150, unique=True, verbose_name='Trading name', blank=False, null=False)
+    situation = models.CharField(max_length=1, choices=SITUATION_CHOICES, default=ACTIVE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    """
+    first_name:
+    last_name:
+    username:
+    company:
+    situation:
+    updated:
+    created:
+    """
+    ACTIVE = 'A'
+    INACTIVE = 'I'
+    EXCLUDED = 'E'
+    SITUATION_CHOICES = [
+        (ACTIVE, 'Active'),
+        (INACTIVE, 'Inactive'),
+        (EXCLUDED, 'Excluded'),
+    ]
+    first_name = models.CharField(max_length=150, verbose_name='First name')
+    last_name = models.CharField(max_length=150, verbose_name='Last name')
+    username = models.CharField(max_length=150, unique=True, verbose_name='Username', blank=False, null=False)
+    company = models.ManyToManyField(Company, related_name='employee')
+    situation = models.CharField(max_length=1, choices=SITUATION_CHOICES, default=ACTIVE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
